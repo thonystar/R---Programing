9a30349af87c05a74550b7df54c02ab5520b5683
Thony <anthony.sulgaray1@gmail.com>
2025-03-15 12:36:05 -0500

    Ultima parte

--- Primeras Clases.R
+++ Primeras Clases.R
@@ -294,4 +294,256 @@ x
                    sep = ",", colClasses = clases)
 data2 
 
+### FORMATO DE DATOS DE TEXTO
+# -- con dput guardas el objeto en R y ademas los metadatos
+d <- data.frame(a = 1, b = 5)
+dput(d)
 
+## Guardarlo en un archivo
+
+dput(d, file = "Rdata.R")
+dget("Rdata.R")
+
+
+## Otra forma es usar la funcion dump
+
+a <- data.frame(a = 1,b = 6,c = 8,d = 6)
+b <- "Hola"
+
+dump(c("a","b"), file = "Prueba2.R")
+
+# Remover los objetos
+rm(a,b)
+
+source("Prueba2.R")
+a
+b
+
+## Leer datos con conexiones
+str(file)
+
+#Objeto de R
+z <- c("Hola","Mundo")
+
+# Crea archivo de txt
+dput(z,"Prueba1.txt")
+
+# Crea una conexion para leer el archivo en R
+conex1 <- file("Prueba1.txt", "r")
+
+# Lee el archivo en con read.csv
+data1 <- read.csv(conex1)
+data1
+
+# Cierra la conexion
+close(conex1)
+
+
+## Para leer una ruta web
+
+conex2 <- url("http://www.jhsph.edu","r")
+
+# con readLines
+x <- readLines(conex2)
+
+head(x,5)
+
+
+## Subsetting
+
+### Para extraer datos dentro de un objeto de R 
+
+
+list3 <- list(a = c(1,6,3,12), b = "Gato", c = c("1","4"))
+
+# Obtiene un elemento
+list3[[1]]
+
+# Obtiene una nueva lista
+list3[1]
+
+# Obtiene un elemento de un objeto respecto al nombre
+list3$b
+
+
+# Se extrae un rango
+
+list3[1:2]
+
+# Usar un indice numerico
+
+a <- c(1,6,9,12,16,21)
+
+a[a > 1] 
+
+# Usar un indice logico
+
+b <- a > 5
+b 
+
+
+## Subconjuntos de Listas
+z <- list(gasto = c(1090,139,12), ingreso = "Nada")
+
+# Diferencia entre z[] y z[[]]
+z[1]
+
+z[[1]]
+
+
+#Si quiero extraer varios elementos:
+
+x <- list(a = 1:4, b = c(TRUE, FALSE), c = "Lost")
+
+# Si se quiere extraer dos elementos 
+
+x[c(1,3)]
+
+# Me da como input del primer y tercer elemento.
+
+## Se puede indexar un elemento de una lista
+x <- list(gas = 1:4, categoria = c(TRUE, FALSE), title = "Lost")
+
+# Si se quiere indexas
+
+a <- "gas"
+
+x[[a]]
+
+# Da como imput el vector 1,2,3,4
+
+## Pero si se quiere indexar con el nombre no se podría
+x$a ## Me sale que no existe un elemento de la lista que se llame *a*
+
+## Se tendría que hacer de forma normal
+x$gas
+
+# Se puede extraer un subelemento de un elemento.
+
+x <- list(gas = list(14,12,13), categoria = c(TRUE, FALSE))
+
+# Si se quiere indexas un subelemento
+
+x[[c(1,3)]]
+
+## El input seria el numero 13, notar que para este caso se toma un doble corchete, 
+## extrayendo el tercer subelemento del primer elemento.
+
+# Otra forma
+
+x[[1]][[3]]
+
+
+## SUBCONJUNTO DE MATRICES
+x <- matrix(1:6,2,3) ## Creamos una matriz de dos filas y tres columnas
+
+## Subconjuntos
+
+x[1,2] # Se extrae el elemento 3
+
+x[2,1] # Se extrae el elemento 2
+
+
+## Se puede extraer una columna o una fila entera
+
+x[1, ] 
+
+# Se extrae la primera fila (1,3,5)
+
+x[ ,2]
+
+# Se extrae la segunda columna (3,4)
+
+## Cuando extraigo solo un elemento de la matriz se convierte en un solo vector. 
+##Pero a veces queremos que nos devuelva una matriz.
+
+
+x <- matrix(1:6,2,3) ## Creamos una matriz de dos filas y tres columnas
+
+## Subconjuntos
+
+x[1,2] # Me da como input 3
+
+# Si quiero que mantenga el formato de matriz
+
+x[1,2, drop = FALSE]
+
+# Subconjunto - Coincidencia Parcial
+
+x <- list(anthony = 1:4)
+
+# Como es complicado a cada rato codificar anthony se puede extraer solo poniendo la a
+
+x$a
+
+# Input
+#(1,2,3,4)
+
+x[["a"]] # Me sale NULL
+
+# Se debe poner la function exact
+
+x[["a", exact = FALSE]]
+
+
+## ELIMINACION DE VALORES NA
+x <- c(1,2,4,NA,6, NA)
+
+a <- is.na(x)
+
+x[!a]
+
+# No arroja un vector sin NA
+
+## Otra forma de realizarlo 
+x <- c(1,NA,3,6,NA)
+u <- c("a",NA,"b","c",NA)
+
+# Se utiliza la funcion
+
+uno <- complete.cases(x,y) ## Arrojando TRUE, FALSE, TRUE, TRUE, FALSE
+
+x[uno]
+
+y[uno]
+
+## Me arroja los vectores sin NA
+
+
+###Para grandes bases de datos
+
+anthony[1:6, ]
+
+# Se tiene varios elementos de una fila y columna en NA
+
+uno <- complete.cases(anthony)
+
+anthony[uno, ][1:6, ]
+
+## Operaciones vectorizadas
+
+## Operaciones con vectores
+x <- c(1,3,5,7); y <- 4:8
+
+## Sumamos
+x + y
+x * y
+x < 2
+x == 2
+x > 4
+x / y
+
+## Para vectores sigue el mismo comportamiento
+
+## Operaciones con vectores
+x <- matrix(1:4,2,2)
+y <- matrix(rep(10,4),2,2)
+
+## Se usa para multiplicacion y division
+
+x * y
+x / y
+
+# Para realizar una verdadera multiplicacion de matrices 
+
+x %*% y
